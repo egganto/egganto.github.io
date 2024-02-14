a887120a1fed52b3d9dad4bb31fc42d47119b8d5
egganto <egganto@gmail.com>
2024-02-15 02:31:46 +0900

    not-so-final edits

--- countdown.js
+++ countdown.js
@@ -1,40 +1,13 @@
-function openingCountdown () {
-    const openingDate = new Date ('February 14, 2024 00:00');
-    const now = new Date ();
-
-    const diff = openingDate - now;
-
-    const mInSecond = 1000;
-    const mInMinute = 60*1000;
-    const mInHour = 60*60*1000;
-    const mInDay = 24*60*60*1000;
-
-const displayDay = Math.floor (diff/mInDay);
-const displayHour = Math.floor ((diff%mInDay)/mInHour);
-const displayMinute = Math.floor ((diff%mInHour)/mInMinute);
-const displaySecond = Math.floor ((diff%mInMinute)/mInSecond);
-
-document.querySelector('.days').textContent = displayDay;
-document.querySelector('.hours').textContent = displayHour;
-document.querySelector('.minutes').textContent = displayMinute;
-document.querySelector('.seconds').textContent = displaySecond;
-
-if (diff<=0) {
-    document.querySelector ('.days'). textContent = 0;
-    document.querySelector ('.hours').textContent = 0;
-    document.querySelector ('.minutes').textContent = 0;
-    document.querySelector ('.seconds').textContent = 0;
-
-    clearInterval(timerID); 
-    reveal();
+function checkPass(){
+  let password = document.getElementById("pswd").value;
+  if (password == "laszloszabo") {
+    window.location="welcome.html";
+  } else {
+    alert("wrong password ^_^ try again! hint: your first name + last name");
+  }
 }
-
-}
-
-let timerID = setInterval (openingCountdown,1000);
-
-openingCountdown ();
 
 function reveal(){
-
+  document.getElementById('main').style.display = 'block';
 };
+
